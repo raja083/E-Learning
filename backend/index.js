e296ab07b7927f940385c5b15ff1a5b86904e702
raja083 <151161871+raja083@users.noreply.github.com>
2025-07-09 16:34:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://e-learning-kgkm.onrender.com",
     credentials: true,
   })
 );
