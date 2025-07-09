fc479f06453a1bd8963850b05db29183e1dfed70
raja083 <151161871+raja083@users.noreply.github.com>
2025-07-09 14:45:14 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://e-learning-frontend-bimw.onrender.com",
     credentials: true,
   })
 );
