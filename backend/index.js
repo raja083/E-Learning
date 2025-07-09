73da3cad1a0fba88a099701132cbe78d4934cdbb
Raja <rajad4096@gmail.com>
2025-07-09 15:30:36 +0530

    change the cors

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://e-learning-frontend-bimw.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
--- frontend/src/features/api/courseProgressApi.js
+++ frontend/src/features/api/courseProgressApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 
-const COURSE_PROGRESS_API = "https://e-learning-backend-lz6k.onrender.com/api/progress"
+const COURSE_PROGRESS_API = "http://localhost:8080/api/progress"
 export const courseProgressApi = createApi({
     reducerPath:"courseProgressApi",
     baseQuery:fetchBaseQuery({
