54fc04db948dcb574eddaa720b34b97db37789e0
Raja <rajad4096@gmail.com>
2025-08-18 14:23:45 +0530

    filter feature corrected

--- frontend/src/pages/student/Filter.jsx
+++ frontend/src/pages/student/Filter.jsx
@@ -13,17 +13,17 @@ import { Separator } from "@/components/ui/separator";
 
 import React, { useState } from "react";
 const categories = [
-  { id: "nextjs", label: "Next JS" },
-  { id: "data science", label: "Data Science" },
-  { id: "frontend development", label: "Frontend Development" },
-  { id: "fullstack development", label: "Fullstack Development" },
-  { id: "mern stack development", label: "MERN Stack Development" },
-  { id: "backend development", label: "Backend Development" },
-  { id: "javascript", label: "Javascript" },
-  { id: "python", label: "Python" },
-  { id: "docker", label: "Docker" },
-  { id: "mongodb", label: "MongoDB" },
-  { id: "html", label: "HTML" },
+  { id: "Next JS", label: "Next JS" },
+  { id: "Data Science", label: "Data Science" },
+  { id: "Frontend Development", label: "Frontend Development" },
+  { id: "Fullstack Development", label: "Fullstack Development" },
+  { id: "MERN Stack Development", label: "MERN Stack Development" },
+  { id: "Backend Development", label: "Backend Development" },
+  { id: "Javascript", label: "Javascript" },
+  { id: "Python", label: "Python" },
+  { id: "Docker", label: "Docker" },
+  { id: "MongoDB", label: "MongoDB" },
+  { id: "HTML", label: "HTML" },
 ];
 const Filter = ({ handleFilterChange }) => {
   const [selectedCategories, setSelectedCategories] = useState([]);
@@ -69,6 +69,7 @@ const Filter = ({ handleFilterChange }) => {
           <div className="flex items-center space-x-2 my-2">
             <Checkbox
               id={category.id}
+              checked={selectedCategories.includes(category.id)}
               onCheckedChange={() => handleCategoryChange(category.id)}
             />
             <Label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed">
